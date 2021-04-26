5d46e1524dec8b90d94818d625058682aeb202d6
zardes42 <53053916+zardes42@users.noreply.github.com>
2021-04-26 11:46:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function open_menu(){
+    document.getElementById('menu-modal').style.display= 'block' ;
+    document.getElementById('small-menu').style.display ='block'; 
+    document.getElementById('close').style.display = 'block';
+}
+function close_menu(){
+    document.getElementById('menu-modal').style.display= 'none' ;
+    document.getElementById('small-menu').style.display ='none'; 
+    document.getElementById('close').style.display = 'none';
+
+}
